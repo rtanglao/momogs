af248e59bd0bb5ea591b3c2248cde0f66b2b49b4
Roland Tanglao <roland@rolandtanglao.com>
2011-05-30 16:37:01 -0700

    add weeklyMetrics.rb to run all the metrics scripts instead of invoking all threee of them tediously by hand

---
+++ weeklyMetrics.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+start_date = ARGV[0] + ARGV[1] + ARGV[2]
+start_date_str = ARGV[0] + " " + ARGV[1] + " " + ARGV[2]
+
+end_date = ARGV[3] + ARGV[4] + ARGV[5]
+end_date_str = ARGV[3] + " " + ARGV[4] + " " + ARGV[5]
+
+stderrfile = 'getactive.' + start_date + '.' + end_date + '.stderr'
+stdoutfile = 'getactive.' + start_date + '.' + end_date + '.stdout'
+`./getactive.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
+stderrfile = 'gethappy.' + start_date + '.' + end_date + '.stderr'
+stdoutfile = 'gethappy.' + start_date + '.' + end_date + '.stdout'
+`./gethappykludged.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
+stderrfile = 'getresolved' + start_date + '.' + end_date + '.stderr'
+stdoutfile = 'getresolved.' + start_date + '.' + end_date + '.stdout'
+`./getNumCreatedNumResolved.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
+
