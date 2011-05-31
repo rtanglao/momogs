e989b378ccfa79d1baf261849d7962d04d19109b
Roland Tanglao <roland@rolandtanglao.com>
2011-05-31 16:30:35 -0700

    add convenience script FINALLY to ease my daily typing :-) emailDailyMetrics.rb

---
+++ emailDailyMetrics.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+t = Time.now
+mm = t.month.to_s
+yyyy = t.year.to_s
+dd = t.day.to_s
+start_date = yyyy + mm + dd
+start_date_str = yyyy  + " " + mm + " " + dd
+
+end_date = start_date
+end_date_str = start_date_str
+
+stderrfile = 'emailReport.' + start_date + '.' + end_date + '.stderr'
+stdoutfile = 'emailReport.' + start_date + '.' + end_date + '.stdout'
+`./emailReport.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
