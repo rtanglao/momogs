4853df4a918e93b1a51d8079b6afa67dbccad2e1
Roland Tanglao <roland@rolandtanglao.com>
2011-06-11 21:54:58 -0700

    simply unsolvedTopicsRepliedToInTheLastTwoWeeksThatIHaveRepliedToWithRepliesAfterMyReply.rb - Only need to reply if the last reply is not by gs_contributor

--- unsolvedTopicsRepliedToInTheLastTwoWeeksThatIHaveRepliedToWithRepliesAfterMyReply.rb
+++ unsolvedTopicsRepliedToInTheLastTwoWeeksThatIHaveRepliedToWithRepliesAfterMyReply.rb
@@ -24,11 +24,11 @@ topicsColl.find({"created_at" => {"$gte" => metrics_start, "$lt" => metrics_stop
                   "status" => { "$nin" => ["complete","rejected"]},
                   "reply_array" => { "$elemMatch"  => { "author.canonical_name" => gs_contributor}}}).each do |t|
   $stderr.printf("CHECKING topic url:%s id:%d which was created at:%s\n",t["at_sfn"],t["id"], t["created_at"].to_s)
-  # search this topics replies to see if there are any replies AFTER gs_contributor_last_reply, if so print out the url
-  last_gs_contributor_reply_time = Time.utc(2009,1,1) # since all topics in the database were created after july 20, 2009
-  newest_reply_time = last_gs_contributor_reply_time
+  # search this topics replies to see if the last reply is by gs_contributor, if it's not then gs_contributor should reply
+  newest_reply_time = Time.utc(2009,1,1) # since all topics in the database were created after july 20, 2009
   last_reply_is_gs_contributor = false
   t["reply_array"].each do |reply|
+    reply_time = reply["created_at"]
     if reply["created_at"].kind_of? String
       $stderr.printf("String reply created_at:%s\n", reply["created_at"])
       reply_time = Time.parse(reply["created_at"])
@@ -41,21 +41,15 @@ topicsColl.find({"created_at" => {"$gte" => metrics_start, "$lt" => metrics_stop
       $stderr.printf("Reply time is greater than newest_reply_time\n")
       newest_reply_time = reply_time
       if reply_author == gs_contributor 
-        if (reply_time <=> last_gs_contributor_reply_time) == 1
-          last_gs_contributor_reply_time = reply_time
-          last_reply_is_gs_contributor = true
-        end
-        next
-      end
-      if (reply_time <=> last_gs_contributor_reply_time) == 1
-          last_gs_contributor_reply_time = reply_time
-          last_reply_is_gs_contributor = false
-      end      
-    end
-  end
+        last_reply_is_gs_contributor = true
+      else 
+        last_reply_is_gs_contributor = false
+      end 
+    end # reply is after newest_reply_time
+  end # reply iterator
   if !last_reply_is_gs_contributor
     printf("Please reply to: %s\n", t["at_sfn"])
   else
     $stderr.printf("No need to reply to: %s\n", t["at_sfn"])
   end
-end
+end # topic iterator
