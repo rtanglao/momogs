461ec53e18e1e998db4a38350f2f367c475121b8
Roland Tanglao <roland@rolandtanglao.com>
2011-06-13 15:48:54 -0700

    add gettopicURLs-m.rb, a mongodb version of gettopicURLs.rb

---
+++ gettopicURLs-m.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'pp'
+require 'date'
+require 'time'
+require 'mongo'
+ 
+if ARGV.length < 6
+  puts "usage: #{$0} yyyy mm dd yyyy mmm dd"
+  exit
+end
+
+metrics_start = Time.utc(ARGV[0], ARGV[1], ARGV[2], 0, 0)
+metrics_stop = Time.utc(ARGV[3], ARGV[4], ARGV[5], 23, 59)
+
+db = Mongo::Connection.new.db("gs") # no error checking  :-) assume Get Satisfaction Database is there on localhost
+topicsColl = db.collection("topics")
+
+topics_array = []
+topicsColl.find({"created_at" => {"$gte" => metrics_start, "$lte" => metrics_stop}
+                }, :fields => ["at_sfn", "id", "created_at"]).sort([["created_at", Mongo::ASCENDING]]).each do |t|
+  url = t["at_sfn"]
+  $stderr.printf("topic:%d, url:%s\n", t["id"],url)
+  topics_array << url
+end
+
+executable_name = $0.gsub(".rb","") 
+printf(STDERR, "CSV filename:%s.%s%s%s.%s%s%s.csv",executable_name,ARGV[0],ARGV[1],ARGV[2],ARGV[3],ARGV[4],ARGV[5])
+ 
+csv_file = File.new(executable_name+"."+ARGV[0]+ARGV[1]+ARGV[2]+"."+ARGV[3]+ARGV[4]+ARGV[5] + ".csv", "w")
+
+topics_array.each{|topic_url|
+  csv_file.puts "#{topic_url}"
+}
+ 
+csv_file.close
