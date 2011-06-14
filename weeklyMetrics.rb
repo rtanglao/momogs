ca50293d903ee489de696f9018c1c942b36f514e
Roland Tanglao <roland@rolandtanglao.com>
2011-06-13 23:22:03 -0700

    update weekly metrics to read from MongoDB instead of GS API

--- weeklyMetrics.rb
+++ weeklyMetrics.rb
@@ -7,11 +7,11 @@ end_date_str = ARGV[3] + " " + ARGV[4] + " " + ARGV[5]
 
 stderrfile = 'getactive.' + start_date + '.' + end_date + '.stderr'
 stdoutfile = 'getactive.' + start_date + '.' + end_date + '.stdout'
-`./getactive.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
+`./getactive-m.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
 stderrfile = 'gethappy.' + start_date + '.' + end_date + '.stderr'
 stdoutfile = 'gethappy.' + start_date + '.' + end_date + '.stdout'
-`./gethappykludged.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
+`./gethappykludged-m.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
 stderrfile = 'getresolved' + start_date + '.' + end_date + '.stderr'
 stdoutfile = 'getresolved.' + start_date + '.' + end_date + '.stdout'
-`./getNumCreatedNumResolved.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
+`./getNumCreatedNumResolved-m.rb #{start_date_str} #{end_date_str} 2>#{stderrfile} 1>#{stdoutfile}`
 
