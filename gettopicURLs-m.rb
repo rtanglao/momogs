7ad3bacc535e1c7b9cf263fafc020494dd43d771
Roland Tanglao <roland@rolandtanglao.com>
2011-08-15 16:58:23 -0700

    change daily report scripts to use environment variables for the mongo server

--- gettopicURLs-m.rb
+++ gettopicURLs-m.rb
@@ -14,7 +14,21 @@ end
 metrics_start = Time.utc(ARGV[0], ARGV[1], ARGV[2], 0, 0)
 metrics_stop = Time.utc(ARGV[3], ARGV[4], ARGV[5], 23, 59)
 
-db = Mongo::Connection.new.db("gs") # no error checking  :-) assume Get Satisfaction Database is there on localhost
+MONGO_HOST = ENV["MONGO_HOST"]
+raise(StandardError,"Set Mongo hostname in  ENV: 'MONGO_HOST'") if !MONGO_HOST
+MONGO_PORT = ENV["MONGO_PORT"]
+raise(StandardError,"Set Mongo port in  ENV: 'MONGO_PORT'") if !MONGO_PORT
+MONGO_USER = ENV["MONGO_USER"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_USER'") if !MONGO_USER
+MONGO_PASSWORD = ENV["MONGO_PASSWORD"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_PASSWORD'") if !MONGO_PASSWORD
+
+db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT.to_i).db("gs")
+auth = db.authenticate(MONGO_USER, MONGO_PASSWORD)
+if !auth
+  raise(StandardError, "Couldn't authenticate, exiting")
+  exit
+end
 topicsColl = db.collection("topics")
 
 topics_array = []
