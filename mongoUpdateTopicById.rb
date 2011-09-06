eeb1a398afc956ccd9ce09630e79c1acf9f9857f
Roland Tanglao <roland@rolandtanglao.com>
2011-09-05 23:52:58 -0700

    update mongoUpdateTopicById.rb to read MONGODB environment variables

--- mongoUpdateTopicById.rb
+++ mongoUpdateTopicById.rb
@@ -39,7 +39,22 @@ if ARGV.length < 1
   exit
 end
 
-db = Mongo::Connection.new.db("gs") # no error checking  :-) assume Get Satisfaction Database is there on localhost
+MONGO_HOST = ENV["MONGO_HOST"]
+raise(StandardError,"Set Mongo hostname in  ENV: 'MONGO_HOST'") if !MONGO_HOST
+MONGO_PORT = ENV["MONGO_PORT"]
+raise(StandardError,"Set Mongo port in  ENV: 'MONGO_PORT'") if !MONGO_PORT
+MONGO_USER = ENV["MONGO_USER"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_USER'") if !MONGO_USER
+MONGO_PASSWORD = ENV["MONGO_PASSWORD"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_PASSWORD'") if !MONGO_PASSWORD
+
+db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT.to_i).db("gs")
+auth = db.authenticate(MONGO_USER, MONGO_PASSWORD)
+if !auth
+  raise(StandardError, "Couldn't authenticate, exiting")
+  exit
+end
+
 topicsColl = db.collection("topics")
 
 id = ARGV[0].to_i
