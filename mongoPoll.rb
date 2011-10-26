75da3371bd67d0dde8ae5dc5b0acff6eef085da2
Roland Tanglao <roland@rolandtanglao.com>
2011-10-26 14:21:35 -0700

    add mongoPoll; a simple GS poller that polls every 15 minutes for topics updated in the last 30 minutes

---
+++ mongoPoll.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'pp'
+require 'time'
+require 'date'
+require 'mongo'
+require 'getGSTopicsAfter'
+
+
+MONGO_HOST = ENV["MONGO_HOST"]
+raise(StandardError,"Set Mongo hostname in  ENV: 'MONGO_HOST'") if !MONGO_HOST
+MONGO_PORT = ENV["MONGO_PORT"]
+raise(StandardError,"Set Mongo port in  ENV: 'MONGO_PORT'") if !MONGO_PORT
+MONGO_USER = ENV["MONGO_USER"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_USER'") if !MONGO_USER
+MONGO_PASSWORD = ENV["MONGO_PASSWORD"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_PASSWORD'") if !MONGO_PASSWORD
+
+db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT.to_i).db("gs")
+auth = db.authenticate(MONGO_USER, MONGO_PASSWORD)
+if !auth
+  raise(StandardError, "Couldn't authenticate, exiting")
+  exit
+end
+
+topicsColl = db.collection("topics")
+
+
+while true
+  metrics_start = Time.now() - (30 * 60) # 30 minutes
+  $stderr.printf("POLLING at:%s going back 30 minutes\n", metrics_start)
+  getGSTopicsAfter(metrics_start, topicsColl)
+  $stderr.printf("SLEEPING at:%s for 15 minutes\n", Time.now)
+  sleep(15 * 60) # 15 minutes
+end
