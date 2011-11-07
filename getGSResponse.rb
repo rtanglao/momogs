fb8a343a7f86ca74beaf87517eff5436a2dd9c56
Roland Tanglao <roland@rolandtanglao.com>
2011-11-07 13:23:11 -0800

    remove retry logic because it is problematic ; better just to stop if there are network timeouts or HTTP non 200 responses

--- getGSResponse.rb
+++ getGSResponse.rb
@@ -4,20 +4,6 @@ require 'nestful'
 
 def getResponse(url, params)
   url = "http://api.getsatisfaction.com/" + url
-  try1 = true
-  begin
-    resp = Nestful.get url, :format => :json, :params => params
-  rescue Nestful::TimeoutError
-    if try1
-      $stderr.printf("retrying after HTTP GET Timeout EXCEPTION, url:%s\n",url)
-      sleep(1)
-      try1 = false
-      retry
-    else
-      $stderr.printf("2nd HTTP GET Failed with a Timeout EXCEPTION, url:%s TERMINATING\n",url)
-      raise
-    end
-  end
-  return resp
+  return Nestful.get url, :format => :json, :params => params
 end
 
