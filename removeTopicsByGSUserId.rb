92f3147753b580ab0271c35f44b1510b7f8167b9
Roland Tanglao <roland@rolandtanglao.com>
2012-06-11 23:14:52 -0700

    add scripts to get and remove topics by GS Userid in order to cope with spammers

---
+++ removeTopicsByGSUserId.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'time'
+require 'date'
+require 'mongo'
+require 'pp'
+
+MONGO_HOST = ENV["MONGO_HOST"]
+raise(StandardError,"Set Mongo hostname in  ENV: 'MONGO_HOST'") if !MONGO_HOST
+MONGO_PORT = ENV["MONGO_PORT"]
+raise(StandardError,"Set Mongo port in  ENV: 'MONGO_PORT'") if !MONGO_PORT
+MONGO_USER = ENV["MONGO_USER"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_USER'") if !MONGO_USER
+MONGO_PASSWORD = ENV["MONGO_PASSWORD"]
+raise(StandardError,"Set Mongo user in  ENV: 'MONGO_PASSWORD'") if !MONGO_PASSWORD
+
+db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT.to_i).db("gs")
+auth = db.authenticate(MONGO_USER, MONGO_PASSWORD)
+if !auth
+  raise(StandardError, "Couldn't authenticate, exiting")
+  exit
+end
+
+topicsColl = db.collection("topics")
+
+if ARGV.length < 1
+  puts "usage: #{$0} [remove topic for this userid]"
+  exit
+end
+
+userid = ARGV[0]
+
+topicsColl.find({"author.canonical_name" => userid},
+                :fields => ["at_sfn", "id", "subject", "content", "author"]
+                ).each do |t|
+  $stderr.printf("Removing id:%d, URL:%s TITLE:%s,FIRST66:%s\n", t["id"], t["at_sfn"],t["subject"], t["content"][0..66])
+  topicsColl.remove("id" => t["id"])
+end
